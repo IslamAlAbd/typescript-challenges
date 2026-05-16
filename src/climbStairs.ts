adc061a2397bca250cd6d240b05fa8f3e528d365
IslamAlAbd <islam.abd.4321@gmail.com>
2026-05-16 15:39:19 +0300

    add the solution of problem five

--- src/climbStairs.ts
+++ src/climbStairs.ts
@@ -19,3 +19,18 @@ const jumpGame = (arr: number[]): boolean => {
 };
 //console.log(jumpGame([2, 3, 1, 1, 4]));
 
+const rodCutting = (n: number, price: number[]): number => {
+  if (n === 0) return 0;
+  //maxprofit[i] stores the maximum profit you can get from a rod of length i
+  const maxProfit = new Array(n + 1).fill(0);
+  maxProfit[0] = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      maxProfit[i] = Math.max(maxProfit[i]!, price[j]! + maxProfit[i - j]!);
+    }
+  }
+
+  return maxProfit[n];
+};
+//console.log(rodCutting(8, [0, 1, 5, 8, 9, 10, 17, 17, 20]));
+
