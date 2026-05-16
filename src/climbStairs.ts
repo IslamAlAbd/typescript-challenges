64b79f07077bec8e07833c3538ec5fa1e569bc9b
IslamAlAbd <islam.abd.4321@gmail.com>
2026-05-16 15:38:34 +0300

    add the solution of problem four

--- src/climbStairs.ts
+++ src/climbStairs.ts
@@ -5,3 +5,17 @@ const climbStairs = (n: number): number => {
 };
 //console.log(climbStairs(6));
 
+const jumpGame = (arr: number[]): boolean => {
+  let maxReach = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i > maxReach) return false;
+    maxReach = Math.max(maxReach, i + arr[i]!);
+    if (maxReach >= arr.length - 1) {
+      return true;
+    }
+  }
+  return false;
+};
+//console.log(jumpGame([2, 3, 1, 1, 4]));
+
