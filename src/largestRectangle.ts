6c02dec1b9a04e4b20e96b7a63d3b35fff84e37d
IslamAlAbd <islam.abd.4321@gmail.com>
2026-05-16 17:29:03 +0300

    add the solution of problem seven

--- src/largestRectangle.ts
+++ src/largestRectangle.ts
@@ -29,4 +29,21 @@ const largestRectangleArea = (heights: number[]): number => {
 
   return maxArea;
 };
-console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]))
+
+const maximalRectangle = (mat: number[][]): number => {
+  const heights = new Array(mat[0]!.length).fill(0);
+  let maxArea = 0;
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[0]!.length; j++) {
+      if (mat[i]![j] == 1) {
+        heights[j] += 1;
+      }
+      if (mat[i]![j] == 0) {
+        heights[j] = 0;
+      }
+    }
+    maxArea = Math.max(maxArea, largestRectangleArea(heights));
+  }
+
+  return maxArea;
+};
