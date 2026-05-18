9ae06df76d1a1c37042137f7833f2ec9d9306edd
IslamAlAbd <islam.abd.4321@gmail.com>
2026-05-18 20:41:38 +0300

    set up debugging tools

--- src/largestRectangle.ts
+++ src/largestRectangle.ts
@@ -5,26 +5,28 @@ const largestRectangleArea = (heights: number[]): number => {
 
   while (index < heights.length) {
     if (
-      stack.length == 0 ||
+      stack.length === 0 ||
       heights[index]! >= heights[stack[stack.length - 1]!]!
     ) {
       stack.push(index);
       index++;
     } else {
       const top = stack.pop()!;
-      const height = heights[top];
+      const height = heights[top]!;
       const width =
         stack.length === 0 ? index : index - stack[stack.length - 1]! - 1;
-      maxArea = Math.max(maxArea, height! * width);
+      maxArea = Math.max(maxArea, height * width);
     }
   }
-  // process remaining bars
+
   while (stack.length > 0) {
     const top = stack.pop()!;
-    const height = heights[top];
+    const height = heights[top]!;
     const width =
-      stack.length === 0 ? index : index - stack[stack.length - 1]! - 1;
-    maxArea = Math.max(maxArea, height! * width);
+      stack.length === 0
+        ? heights.length
+        : heights.length - stack[stack.length - 1]! - 1;
+    maxArea = Math.max(maxArea, height * width);
   }
 
   return maxArea;
